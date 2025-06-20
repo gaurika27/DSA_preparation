10c8d0eea0a1c32e6bf23567600d48ba104ac7a9
Gaurika Sharma <gaurikasharma27@gmail.com>
2025-06-20 20:09:32 +0530

    tenth

---
+++ Google/53_Maximum Subarray.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currsum=0, maxsum=INT_MIN;
+        for(auto it:nums){
+            currsum+=it;
+            maxsum=max(currsum,maxsum);
+            if(currsum<0) currsum=0;
+        }        
+        return maxsum;
+    }
+};
