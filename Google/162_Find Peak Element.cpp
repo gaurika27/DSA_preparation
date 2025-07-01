155ce8723854cb061680e317af8eb74872c1e4f9
Gaurika Sharma <gaurikasharma27@gmail.com>
2025-07-02 04:10:32 +0530

    nineteenth

---
+++ Google/162_Find Peak Element.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int an=nums.size(), low=0, high=nums.size()-1;
+        while(low<high){
+            int mid=(low+high)>>1;
+            if(nums[mid]>nums[mid+1]) high=mid;
+            else low=mid+1;
+        }
+        return low;        
+    }
+};
